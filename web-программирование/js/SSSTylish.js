4fc29e59f17139b140e78ee78df9df29e1806379
Serg <100794207+Senchakov26@users.noreply.github.com>
2025-03-26 13:30:36 +0300

    Add files via upload
    Демонстрационная Web-страница.

---
+++ web-программирование/js/SSSTylish.js
@@ -0,0 +1,14 @@
+window.onload = function (){
+ nav.style.left = "10%";
+ nav.style.width = "90%";
+ header.style.right="0%";
+ header.style.width="90%";
+ g1.style.right="0%";
+ g1.style.width="100%";
+ g2.style.left="0%";
+ g2.style.width="100%";
+ g3.style.right="0%";
+ g3.style.width="100%";
+ g4.style.left="0%";
+ g4.style.width="100%"; 
+}
